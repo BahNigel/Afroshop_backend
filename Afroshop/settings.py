f68f97cc7637615bec21303c1d7d6f1e19b1210d
Bah Nigel <bah.nigel@yahoo.com>
2025-02-22 12:15:51 +0100

    made changes

--- Afroshop/settings.py
+++ Afroshop/settings.py
@@ -134,7 +134,7 @@ AWS_QUERYSTRING_AUTH = False  # Removes authentication query params from URLs
 
 # Static files (CSS, JavaScript, etc.)
 STATICFILES_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
-STATIC_URL = f"https://{AWS_S3_CUSTOM_DOMAIN}/static/"
+# STATIC_URL = f"https://{AWS_S3_CUSTOM_DOMAIN}/static/"
 
 # Media files (Uploaded files)
 DEFAULT_FILE_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
